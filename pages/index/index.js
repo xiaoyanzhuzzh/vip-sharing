729e1736bd246a8e4bba3f28b25747d15e2b39f6
Zhihui Zhang <zhzhang@thoughtworks.com>
2017-05-17 14:34:38 +0800

    Creating index page.

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,22 +4,13 @@ var app = getApp()
 Page({
   data: {
     motto: '奔跑吧，我的会员卡',
+    mottoSubtitle: 'VIP Sharing是一个免费分享各种会员账号微信小程序，我们不以营利为目的，分享、免费、互助是我们的服务宗旨。',
     userInfo: {}
   },
   //事件处理函数
   bindViewTap: function() {
     wx.navigateTo({
       url: '../logs/logs'
-    })
-  },
-  gotoShowPage: function() {
-    wx.navigateTo({
-      url: '../logs/logs',
-    })
-  },
-  gotoAddPage: function() {
-    wx.navigateTo({
-      url: '../logs/logs',
     })
   },
   onLoad: function () {
@@ -31,6 +22,17 @@ Page({
       that.setData({
         userInfo:userInfo
       })
+    })
+  },
+  addVIPInfo: function () {
+    wx.navigateTo({
+      url: '../add/add'
+    })
+  },
+
+  showVIPInfo: function () {
+    wx.navigateTo({
+      url: '../show/show'
     })
   }
 })
