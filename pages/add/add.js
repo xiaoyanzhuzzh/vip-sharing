fd6448c2449235b61c569ed437ba1e830fd14cea
Xin Tong <xin.tong@rea-group.com>
2017-05-18 10:28:21 +0800

    changing the style of add page

--- pages/add/add.js
+++ pages/add/add.js
@@ -1,4 +1,5 @@
 // input.js
+var app = getApp()
 Page({
   data: {
     categories: [
@@ -7,26 +8,19 @@ Page({
       { name: "玩", id: 2 },
       { name: "乐", id: 3 }
     ],
-    cardInfo: {
-      category: { name: '吃', id: 0 },
-      cardNumber: "",
-      cardName: ""
-    }
+    category:{name:"吃",id:0},
+    userInfo: {}
   },
 
-  bindCardNumberInput: function (e) {
-    this.setData({
-      cardInfo: {
-        cardNumber: e.detail.value
-        }
-    })
-  },
-
-  bindCardNameInput: function (e) {
-    this.setData({
-      cardInfo: {
-        cardName: e.detail.value
-      }
+  onLoad: function () {
+    console.log('onLoad')
+    var that = this
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function (userInfo) {
+      //更新数据
+      that.setData({
+        userInfo: userInfo
+      })
     })
   },
 
@@ -44,6 +38,13 @@ Page({
   formSubmit: function (e) {
     const result = e.detail.value
     console.log('form发生了submit事件，携带数据为：', e.detail.value)
+  },
+
+  bindPickerChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index: e.detail.value
+    })
   },
 
   formReset: function () {
