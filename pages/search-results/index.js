54f9464cd5cf02843faba76b2bf38cf26057fda9
Zhihui Zhang <zhzhang@thoughtworks.com>
2018-01-18 13:48:20 +0800

    Display searched Vip results.

--- pages/search-results/index.js
+++ pages/search-results/index.js
@@ -1,6 +1,13 @@
 const AV = require('../../lib/av-weapp-min.js');
 var app = getApp();
 Page({
+  data: {
+    indicatorDots: true,
+    vertical: false,
+    autoplay: true,
+    interval: 3000,
+    duration: 1200
+  },
   onLoad: function (options) {
     var that = this;
     app.getUserInfo(function (userInfo) {
@@ -11,14 +18,18 @@ Page({
     });
   },
   onShow: function () {
+    var that = this;
     var membershipQuery = new AV.Query('Membership');
     membershipQuery.contains('shopName', this.data.currentShop);
     membershipQuery.find().then(function (results) {
+      var attributes = results.map(function (result) {
+        return result.attributes;
+      });
       that.setData({
-        vipResults: results.attributes
+        vipResults: attributes
       })
     }, function (error) {
-      console.log("search failed")
+      console.log("search failed:" + error)
     });
   },
   searchByShopName: function (e) {
